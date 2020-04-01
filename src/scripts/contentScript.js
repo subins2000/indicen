fe19bb67cbd093a04b65de33d5df24015bc811fd
Subin Siby <mail@subinsb.com>
2020-04-01 13:11:50 +0530

    Detect lang before transliteration to increase performance

--- src/scripts/contentScript.js
+++ src/scripts/contentScript.js
@@ -10,6 +10,31 @@
 import browser from 'webextension-polyfill';
 import Transliterator from 'libindic-transliteration';
 
+
+/**
+ * Check if input has characters from a particular language
+ * @param str input Input string
+ * @param str lang Language to check against
+ */
+function has_lang(input, lang) {
+  let charCode = 0,
+      langCodes = {
+        'ml': [3328, 3455], // 0x0D00 to 0x0D7F
+        'hi': [2304, 2431], // 0x0900 to 0x097F
+        'kn': [3200, 3327] // 0x0C80 to 0x0CFF
+      },
+      start = langCodes[lang][0],
+      end = langCodes[lang][1];
+
+  for (let i = 0; i < input.length; i++) {
+    charCode = input.charAt(i).charCodeAt();
+    if (charCode >= start && charCode <= end) {
+      return true;
+    }
+  }
+  return false;
+}
+
 function transliterate_elem_content(elem, lang) {
   const t = new Transliterator();
 
@@ -29,7 +54,14 @@ function transliterate_elem_content(elem, lang) {
 
       //  Make sure this is a text node
       if (node.nodeType === Element.TEXT_NODE || node.nodeType === Element.DOCUMENT_NODE){
-        node.textContent = transliterate(node.textContent);
+        /**
+         * Only does transliteration if it has characters from the lang
+         * Checking for best performance
+         */
+        console.log(has_lang(node.textContent, lang));
+        if (has_lang(node.textContent, lang)) {
+          node.textContent = transliterate(node.textContent);
+        }
       } else if (node.nodeType === Element.ELEMENT_NODE){
         //  Check this node's child nodes for text nodes to act on
         replace_text_in_node(node);
@@ -51,6 +83,7 @@ browser.storage.sync.get('auto').then((result) => {
   if (result.auto) {
     let lang = 'ml';
     browser.storage.sync.get('lang').then((result) => {
+      console.log(result);
       lang = result.lang;
       transliterate_elem_content(document.body, result.lang);
     });
